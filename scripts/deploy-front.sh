954c30cb7ac28d976359964ca251662afdb3dd5c
Clorennec <louis.clorennec@bnli.fr>
2024-02-16 17:43:55 +0100

    last

--- scripts/deploy-front.sh
+++ scripts/deploy-front.sh
@@ -13,12 +13,10 @@ echo "1. Récupération depuis GitHub..."
 bash ./pull-code.sh
 check_status
 
-# Exécuter le script de construction Docker
 echo "2. Construction de l'image Docker..."
 bash ./create-docker-image.sh
 check_status
 
-# Exécuter le script de lancement du conteneur Docker
 echo "3. Lancement du conteneur Docker..."
 bash ./run-docker-container.sh
 check_status
